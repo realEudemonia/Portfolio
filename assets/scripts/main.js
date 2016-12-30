7f5f4e00ce1758408de2d973812af78302a8145f
Jason <nicholsonj863@gmail.com>
2016-12-29 21:55:35 -0500

    Solved jQuery issue, matter of syntax. Added footer and social glyphicons. Just need to style the form and plug in the href URLs for the socials.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,11 +2,32 @@
  * Created by jason on 12/12/16.
  */
 
+
+
 $("#aboutBtn").click(function () {
-    $('html, body').animate({
-            scrollTop: $("#about").offset().top
-        },
-        1000
-    )
+    $("body").scrollTop(675)
 });
 
+$("#portfolioBtn").click(function () {
+    $("body").scrollTop(1000)
+});
+
+$("#contactBtn").click(function () {
+    $("body").scrollTop(2000)
+});
+
+
+
+
+
+// $("#aboutBtn").click(function () {
+//     $("body").animate({scrollTop: $("#about")}, 500)
+// });
+//
+// $("#portfolioBtn").click(function () {
+//     $("body").animate({scrollTop: $("#portfolio").offset().top}, 500)
+// });
+//
+// $("#contactBtn").click(function () {
+//     $("body").animate({scrollTop: $("#contact").offset().top}, 500)
+// });
